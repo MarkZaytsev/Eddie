b333f51a45a959fdb639c2a2841faa1351b81d0f
MarkZaytsev <markzaytsevdev@gmail.com>
2025-10-23 10:49:37 +0700

    RoundTripRequest without result added

--- Assets/Scripts/Eddie/EventDispatching/Events/RoundTripRequest.cs
+++ Assets/Scripts/Eddie/EventDispatching/Events/RoundTripRequest.cs
@@ -2,6 +2,24 @@
 
 namespace Eddie.EventDispatching.Events
 {
+    public abstract class RoundTripRequest : IEvent
+    {
+        public abstract EventType Type { get; }
+
+        private readonly Action _ok;
+        private readonly Action<string> _err;
+
+        protected RoundTripRequest(Action ok, Action<string> err = null)
+        {
+            _ok = ok;
+            _err = err;
+        }
+
+        public void Ok() => _ok?.Invoke();
+
+        public void Err(string message) => _err?.Invoke(message);
+    }
+
     public abstract class RoundTripRequest<T> : IEvent
     {
         public abstract EventType Type { get; }
